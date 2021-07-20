e667d73c63991c827800ef1b41eefba46bba7966
VictoriaColone <1449515567@qq.com>
2021-07-20 23:43:54 +0800

    Android端下载能力实现，暂时失败，虚拟机无网络权限

--- app/src/main/java/com/example/infinitelyflu_mobile/ui/FetchTemplateActivity.java
+++ app/src/main/java/com/example/infinitelyflu_mobile/ui/FetchTemplateActivity.java
@@ -2,6 +2,8 @@ package com.example.infinitelyflu_mobile.ui;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +12,13 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.example.infinitelyflu_mobile.R;
+import com.example.infinitelyflu_mobile.utils.FileUtils;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
 /**
@@ -49,6 +58,18 @@ public class FetchTemplateActivity extends AppCompatActivity {
                 if (!TextUtils.isEmpty(templateName) && !TextUtils.isEmpty(templateVersion)) {
                     Log.d(TAG, templateName + "   " + templateVersion);
                     // yutao todo 调用后端下载接口
+                    FileUtils.downloadFileAsync(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            mLoadingProgressBar.setVisibility(View.GONE);
+                            Toast.makeText(getBaseContext(), "Download Success", Toast.LENGTH_LONG).show();
+                        }
+                    });
                 } else {
                     mLoadingProgressBar.setVisibility(View.GONE);
                     Toast.makeText(getBaseContext(), "Input Expection!", Toast.LENGTH_LONG).show();
---
+++ app/src/main/java/com/example/infinitelyflu_mobile/utils/FileUtils.java
@@ -0,0 +1,71 @@
+package com.example.infinitelyflu_mobile.utils;
+
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class FileUtils {
+
+    private static final String TAG = "FileUtils";
+
+    public static void downloadFileAsync(Callback callback) {
+        Log.d(TAG, "开始下载");
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/InfinitelyFlu_server_war/template/downloadTemplate?name=test&&version=0.1")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG, "下载失败" + e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    //回调的方法执行在子线程。
+                    Log.d(TAG, "onResponse: 获取数据成功了");
+                    try{
+                        //从服务器得到输入流对象
+                        InputStream is = response.body().byteStream();
+                        File dir = new File(String.valueOf(Environment.getExternalStorageDirectory()));
+                        if (!dir.exists()){
+                            dir.mkdirs();
+                        }
+                        //根据目录和文件名得到file对象
+                        File file = new File(dir, response.header("Disposition", "main_android.xml"));
+                        FileOutputStream fos = new FileOutputStream(file);
+                        byte[] buf = new byte[1024*8];
+                        int len = 0;
+                        while ((len = is.read(buf)) != -1){
+                            fos.write(buf, 0, len);
+                        }
+                        fos.flush();
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Log.d(TAG, "run: mainthread");
+                                    callback.onResponse(call, response);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+}
